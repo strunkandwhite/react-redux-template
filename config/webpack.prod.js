843fc4b6a081f26d99a035326ea9f621b7ff71c9
Jack Lenehan <jack.lenehan@gmail.com>
2018-12-09 17:33:19 -0600

    Add travis (#3)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
@@ -9,17 +8,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        parallel: true,
-        uglifyOptions: {
-          compress: {
-            inline: false,
-          },
-        },
-      }),
-      new OptimizeCssAssetsPlugin({}),
-    ],
+    minimizer: [new OptimizeCssAssetsPlugin({})],
   },
   stats: {
     chunks: false,
